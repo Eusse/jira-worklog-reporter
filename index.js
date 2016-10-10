bb3fa3a6f6eaf74f2daad3341d32a6417cec486e
Andres Eusse <ahernandeze@psl.com.co>
2016-10-10 11:58:49 -0500

    Added config detection and jira connection using it

--- index.js
+++ index.js
@@ -5,24 +5,47 @@ inquirer = require('inquirer'),
 fs = require("fs"),
 JiraClient = require('jira-connector');
 
-fs.exists('config.json', (exists) => {
-  if(exists){
-    console.log('Configuration file found. Loading settings.');
-    var content = fs.readFileSync("config.json");
-    console.log(content);
-  }else{
-    console.log('No configuration file found. Starting to create one interactively');
-    var questionsfile = fs.readFileSync("config-generator.json");
-    let config = JSON.parse(questionsfile);
-    inquirer.prompt(config.questions).then(function (answers) {
-      let authToken = new Buffer(`${answers.user}:${answers.pass}`).toString('base64');
-      console.log(authToken);
-      var jira = new JiraClient( {
-          host: answers.host,
-          basic_auth: {
-            base64: authToken
-          }
-      });
+var config = {};
+try {
+  let file = fs.readFileSync("config.json");
+  config = JSON.parse(file);
+  try{
+    var jira = new JiraClient({
+      host: config.host,
+      basic_auth: {
+        base64: config.authToken
+      }
     });
+    try {
+      jira.search.search({
+        jql: 'assignee=currentuser()'
+      }, function(error, issue) {
+        console.log(issue);
+      });
+    }catch(error){
+      console.log('Failed to query for yout issues');
+      console.log('Error details: ' + error.message);
+    }
+  } catch (error){
+    console.log('Config file could be corrupted. Run the command again with the --config option.');
+    console.log('Error details: ' + error.message);
   }
-});
+} catch (e) {
+  console.log('No configuration file found. Starting to create one interactively');
+  var questionsfile = fs.readFileSync("config-generator.json");
+  let config = JSON.parse(questionsfile);
+  var configContent = {};
+  inquirer.prompt(config.questions).then(function (answers) {
+    let authToken = new Buffer(`${answers.user}:${answers.pass}`).toString('base64');
+    configContent = {
+      'host' : answers.host,
+      'authToken': authToken
+    };
+    let buffer = new Buffer(JSON.stringify(configContent));
+    fs.writeFileSync(`${__dirname}/config.json`, buffer, {'flag': 'w+'}, (error) => {
+      if (error) throw err;
+      console.log('Config file saved');
+    });
+    console.log('Configuration complete. Run the command again for more options');
+  });
+}
