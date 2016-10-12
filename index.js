504afed7c660acfca67ff7b1ec3701513e361a7b
Andres Eusse <ahernandeze@psl.com.co>
2016-10-12 11:10:28 -0500

    Added recursive options menu for issue configuration

--- index.js
+++ index.js
@@ -5,26 +5,30 @@ inquirer = require('inquirer'),
 fs = require("fs"),
 JiraClient = require('jira-connector');
 
-var config = {};
+var config = {}, issues, optionsQuestion;
 try {
   let file = fs.readFileSync("config.json");
   config = JSON.parse(file);
+  console.log('Configuration file found');
   try{
     var jira = new JiraClient({
       host: config.host,
+      protocol: config.protocol,
+      path_prefix: config.path_prefix,
+      port: config.port,
+      apiVersion: config.apiVersion,
       basic_auth: {
         base64: config.authToken
       }
     });
-    try {
-      jira.search.search({
-        jql: 'assignee=currentuser()'
-      }, function(error, issue) {
-        console.log(issue);
-      });
-    }catch(error){
-      console.log('Failed to query for yout issues');
-      console.log('Error details: ' + error.message);
+    console.log('Loaded authentication from configuration file');
+    if(config.issues){
+      console.log('Posting time to your issues');
+    }else{
+      console.log('No issues configured. Starting interactive configuration');
+      var issuesQuestions = fs.readFileSync("config-generator.json");
+      optionsQuestion = JSON.parse(issuesQuestions).issues.options;
+      inquirer.prompt(optionsQuestion).then(askOrSaveIssues);
     }
   } catch (error){
     console.log('Config file could be corrupted. Run the command again with the --config option.');
@@ -32,13 +36,17 @@ try {
   }
 } catch (e) {
   console.log('No configuration file found. Starting to create one interactively');
-  var questionsfile = fs.readFileSync("config-generator.json");
-  let config = JSON.parse(questionsfile);
+  var authQuestions = fs.readFileSync("config-generator.json");
+  let config = JSON.parse(authQuestions);
   var configContent = {};
-  inquirer.prompt(config.questions).then(function (answers) {
+  inquirer.prompt(config.auth).then(function (answers) {
     let authToken = new Buffer(`${answers.user}:${answers.pass}`).toString('base64');
     configContent = {
       'host' : answers.host,
+      'protocol': answers.protocol,
+      'port': answers.port,
+      'path_prefix': answers.prefix,
+      'apiVersion': answers.version,
       'authToken': authToken
     };
     let buffer = new Buffer(JSON.stringify(configContent));
@@ -48,4 +56,18 @@ try {
     });
     console.log('Configuration complete. Run the command again for more options');
   });
+}
+
+
+function saveIssues(){
+  console.log('Finished asking');
+}
+
+function askOrSaveIssues(answer) {
+  if (answer.option == 'cancel') {
+    saveIssues();
+    return;
+  }else{
+    inquirer.prompt(optionsQuestion).then(askOrSaveIssues);
+  }
 }
